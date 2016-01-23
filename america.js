4363f42e8d542d7ec13331d136548e487166307c
Ashlin <ashlinaronin@gmail.com>
2016-01-22 18:03:43 -0800

    Remove dummy routes

--- america.js
+++ america.js
@@ -6,18 +6,9 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
-
 // Routes
 app.get('/getTrumpCount', function(request, response) {
     response.send(twitterController.getCurrentData());
-});
-
-app.get('/america', function(request, response) {
-    response.send({america:'good'});
-});
-
-app.get('food', function(request, response) {
-    response.send({hungry:'yes'});
 });
 
 app.listen(5000, '127.0.0.1');
