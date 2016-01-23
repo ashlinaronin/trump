5f720496844f196f5b82e2714bc006c46ab621f7
Ashlin <ashlinaronin@gmail.com>
2016-01-22 16:18:16 -0800

    Make sure to serve at 127.0.0.1 explicitly

--- america.js
+++ america.js
@@ -4,7 +4,6 @@ twitterController.init();
 // Set up server
 var express = require('express');
 var app = express();
-app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 
@@ -13,6 +12,5 @@ app.get('/getTrumpCount', function(request, response) {
     response.send(twitterController.getCurrentData());
 });
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+app.listen(5000, '127.0.0.1');
+console.log('Server is running at 127.0.0.1:5000');
